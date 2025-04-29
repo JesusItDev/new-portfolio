9c275683908cca58ea42c6d0220191d31e5121ae
Jesus Rodriguez <jesus.it.dev@gmail.com>
2025-04-28 23:21:11 -0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import { jetbrainsmono } from "./layout";
+export default function Home() {
+  return (
+    <div className={`${jetbrainsmono.variable}`}>
+      <p className="font-main">homepage</p>
+    </div>
+  );
+}
