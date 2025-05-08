75f7cbb87335750959188e4e6ed3ace008c468f3
Jesus Rodriguez <jesus.it.dev@gmail.com>
2025-05-07 23:50:53 -0600

    Mobile navbar update
    -Removed border
    -Added Links
    -Added styles
    -Added BG

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${jetbrainsmono.variable}`}>
+      <body className={jetbrainsmono.variable}>
         <div className="font-main">
           <Header />
           <TransitionEffect />
--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -15,13 +15,49 @@ const links = [
     path: "/about",
   },
   {
-    name: "resume",
+    name: "Resume",
     path: "/resume",
   },
 ];
 
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  const pathname = usePathname();
+
+  return (
+    <>
+      <Sheet>
+        <SheetTrigger className="flex justify-center items-center">
+          <CiMenuFries className="text-3xl text-primary-orange" />
+        </SheetTrigger>
+        <SheetContent className="flex flex-col">
+          <div className="mt-32 mb-40 text-center text-2xl">
+            <Link href="/">
+              <h1 className="text-2xl font-bold">
+                Jesus<span className="text-primary-orange">.</span>It
+                <span className="text-primary-orange">.</span>Dev
+              </h1>
+            </Link>
+          </div>
+          <nav className="flex flex-col justify-center items-center gap-8">
+            {links.map((link, index) => {
+              return (
+                <Link
+                  href={link.path}
+                  key={index}
+                  className={`${
+                    link.path === pathname &&
+                    "text-primary-orange border-b-2 border-primary-orange text-xl"
+                  } text-xl capitalize hover:text-primary-orange font-medium transition-all duration-300 ease-in-out`}
+                >
+                  {link.name}
+                </Link>
+              );
+            })}
+          </nav>
+        </SheetContent>
+      </Sheet>
+    </>
+  );
 };
 
 export default MobileNav;
