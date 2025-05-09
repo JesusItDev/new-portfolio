51429f9c96439a3cf5667ef54a51a0cffb4c7aa8
Jesus Rodriguez <jesus.it.dev@gmail.com>
2025-05-08 19:26:11 -0600

    Added CodeLineRetriever component
    -Implemented conection and basic functionality with the github API.
    Further implementations are nedded

--- components/CodeLineRetriever.tsx
+++ components/CodeLineRetriever.tsx
@@ -1,13 +1,21 @@
 import { Octokit } from "octokit";
 
 const API_KEY = process.env.API_KEY;
+const OWNER = process.env.OWNER;
 
 const octokit = new Octokit({
   auth: API_KEY,
 });
 
 export default async function CodeLineRetriever() {
-  const data = await fetch("https://api.vercel.app/blog");
-  const posts = await data.json();
-  return <ul></ul>;
+  const data = await octokit.request("GET /users/JesusItDev/repos", {
+    //   owner: "github",
+    //   repo: "docs",
+    //   per_page: 2,
+    // sort: "updated",
+  });
+  {
+    console.log("Response: ", data.data);
+  }
+  return <p>{data.status}</p>;
 }
